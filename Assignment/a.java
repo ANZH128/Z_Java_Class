f566e04e72cab22b3bd6913c7db56b85cd2f85f3
Frank-Zhai <39228176+Frank-Zhai@users.noreply.github.com>
2018-05-13 09:09:45 +0800

    Create a.java

---
+++ Assignment/a.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Scanner stdin = new Scanner(System.in);
+String inString;
+int i, num,sum = 0;
+for (i=1;i<=3;i++) {
+System.out.println("Enter mark of assign"+i+":");
+inString = stdin.next();
+num = Integer.parseInt(inString);
+sum += num;
+}
+System.out.println("The average is"+ (double) sum /3);	
+	}
+}
+	
+	
