3a308c8bfbc6a97f5a484a365ce8096c57f129d6
ANZH <tony.j.tang@outlook.com>
2018-05-27 11:38:58 +1000

    Create TestArray.java

---
+++ week7/TestArray.java
@@ -0,0 +1,12 @@
+public class TestArray {
+   public static void main(String[] args) {
+      double[] myList = {1.9, 2.9, 3.4, 3.5};
+ 
+      // 打印所有数组元素
+      
+      // 计算所有元素的总和
+      
+      // 查找最大元素
+
+   }
+}
