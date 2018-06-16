3f05975f4da154561d692f71b6e51be3355653a9
ANZH <tony.j.tang@outlook.com>
2018-06-16 12:02:40 +0800

    Create 课后作业.java

---
+++ week8/课后作业.java
@@ -0,0 +1,25 @@
+Tutorial Objectives.
+To examine algorithms for accessing 1D arrays of reference variables and 2D arrays
+Tutorial Activities
+ 1. The class Product is described below. Write a code segment for an array called
+ productCatalogue to reference 10 products, read in product data, then display it
+ and then find the product number(s) of products costing less than $20
+
+ 2. How many elements does the array int x[][] = new int[4][5] contain?
+ 3. Design a program to read integers into an integer array of size [3][3]. Treat the
+ array as a 3 * 3 image. For example, it may contain the image:
+ 123
+ 456
+ 789
+ Your program should then copy the array into a new array such that the image is
+ mirrored. For example, the above image would become:
+ 321
+ 654
+ 987
+ This new image would then be printed out.
+ 4. Now modify your algorithm to rotate the original image 180 degrees.
+ 123 becomes 987
+ 456 654
+ 789 321
+5. Design a program that reads integers into an array of size[4][4], and then
+ calculates if the smallest row total equals the smallest column total.
