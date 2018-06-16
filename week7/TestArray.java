1fa4abdbb82f1336f2a70b826ae82433b6a34f30
ANZH <tony.j.tang@outlook.com>
2018-06-16 09:44:28 +0800

    Update TestArray.java

--- week7/TestArray.java
+++ week7/TestArray.java
@@ -1,12 +1,32 @@
 public class TestArray {
-   public static void main(String[] args) {
-      double[] myList = {1.9, 2.9, 3.4, 3.5};
- 
-      // 打印所有数组元素
-      
-      // 计算所有元素的总和
-      
-      // 查找最大元素
-
-   }
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          double[] mylist = {1.9 , 2.9 , 3.4 , 3.5};
+          //打印所有元素
+          for(int i = 0; i < mylist.length;i++){
+              System.out.println(mylist[i]);
+          }
+         
+          //计算元素的合
+          Double temp = 0.0;
+          for(int i = 0; i < mylist.length;i++){
+              temp = temp + mylist[i];
+          }
+          System.out.println(temp);
+		
+	  // 查找最大元素和最小元素
+          double max = mylist[0];
+          double min = mylist[0];
+             for(int i = 0; i < mylist.length;i++){
+              if(max<mylist[i]){
+                  max = mylist[i];
+              }
+              
+              if(min>mylist[i]){
+                  min=mylist[i];
+              }
+          }
+          
+             System.out.println("最大值是"+max+" 最小值是"+min);      
+ }
 }
