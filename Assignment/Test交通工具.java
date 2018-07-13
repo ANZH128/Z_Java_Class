41af5cbb28d252b5229ff296b0dbff5c5566caa4
Frank-Zhai <39228176+Frank-Zhai@users.noreply.github.com>
2018-07-13 16:26:52 +0800

    Create Test交通工具.java

---
+++ Assignment/Test交通工具.java
@@ -0,0 +1,16 @@
+package Transport;
+
+public class Testtransport {
+		 
+		public static void main(String[] args) {
+			Car car = new Car();    
+			Train train = new Train();
+      Plane plane =new Plane();
+		
+ //调用方法	
+			car.Moving();
+			train.Moving();
+			plane.Moving();
+		}
+	 
+	}
